54c0ce697005d60a81c1e3515bd784e7a3a599d5
Poonam67-s <poonam.kalra@successive.tech>
2023-01-02 21:02:54 +0530

    new add commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_demo/provider/movie_provider.dart';
 import 'package:provider_demo/screens/home_screen.dart';
-
 import 'constants/app_constants.dart';
+import 'screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,15 +12,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
-            create: (_)=>MovieProvider(),
-            child: const HomeScreen()),
-        
+            create: (_) => MovieProvider(), child: const HomeScreen()),
       ],
       child: MaterialApp(
         title: AppConstants.appTitleString,
@@ -28,36 +25,8 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-       // home: user != null || isLinkedInLoggedIn == true || isFacebookLoggedIn == true ? const HomeScreen(): const LoginScreen(),
-        
-         home: const HomeScreen(),
-      
-        //builder: EasyLoading.init(),
+        home: const SplashScreen(),
       ),
     );
-    
-    
-    
-    // ChangeNotifierProvider(
-    //   create: (context) {
-    //     MovieProvider();
-    //   },
-    //   builder: (context, child) {
-    //     return MaterialApp(
-    //     title: 'Provider Demo',
-    //     theme: ThemeData(
-    //       primarySwatch: Colors.blue,
-    //     ),
-    //     home: const HomeScreen(),
-    //   );
-    //   },
-    //   child: MaterialApp(
-    //     title: 'Provider Demo',
-    //     theme: ThemeData(
-    //       primarySwatch: Colors.blue,
-    //     ),
-    //     home: const HomeScreen(),
-    //   ),
-    // );
   }
 }
